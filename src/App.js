94c6044e549f960ea7935e43f26c058e0ac65ec3
Isk98 <50041008+Isk98@users.noreply.github.com>
2019-05-01 12:52:16 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -15,21 +15,28 @@ class App extends React.Component{
 
   getWeather = async (e)=>{
     e.preventDefault();
+    e.persist();
+    this.setState({
+      city:e.target.value,
+      country:e.target.value,
+    })
+    console.log(this.state); // TODO: Update state 
+    const baseUrl = "http://api.openweathermap.org/data/2.5/weather"
+    const appId = "9ddad1106126f78a603385086f9e4bf1"
+
+    const api_call = await fetch(`${baseUrl}?q=${this.state.city},${this.state.country}&appid=${appId}`)
+    const data = await api_call.json();
     
-   
-    const api_call= await fetch("http://api.openweathermap.org/data/2.5/weather?q={this.state.city},{this.state.country}&appid=9ddad1106126f78a603385086f9e4bf1")
-   const data = await api_call.json();
-   console.log(data);
+    console.log(data);
+
  };
 
-
+ 
     render(){ 
       return (
         <div>
           <Titles/>
-          <Form getWeather={this.getWeather}
-                getInputValue={this.getInputValue}
-               />
+          <Form getWeather={this.getWeather} />
           <Weather temperature= {this.state.temperature}
                     city =  {this.state.city}
                       country=   {this.state.country} 
