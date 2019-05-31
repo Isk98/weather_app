524d87bb9fdde9660212eb8fa278b70158988945
isk98 <ilvasikurti98@gmail.com>
2019-05-31 09:12:15 +0200

    add map function

--- src/Components/WeatherOfWeek.js
+++ src/Components/WeatherOfWeek.js
@@ -6,61 +6,25 @@ class WeatherOfWeek extends React.Component{
     
     render(){
         
-      const data= [];
+          const weatherItems=this.props.week_data.map((weatherItem) => <Weather key={weatherItem.weekday}
+                                                                                temperature={weatherItem.temperature}
+                                                                                day={weatherItem.weekday}
+                                                                                humidity={weatherItem.humidity}
+                                                                                description={weatherItem.description}
+                                                                                className="all_week" />)
+    
+      
        
-        return(
-            <div >
-                <div className="first-row">
-                <div className="ww-div">
-              <Weather className="ww" temperature={this.props.week_data[0].temperature}
-                                       day={this.props.week_data[0].weekday}
-                                       humidity={this.props.week_data[0].humidity}
-                                       description={this.props.week_data[0].description}/> 
-                </div>
-              <div className="ww-div">
-              <Weather className="ww" temperature={this.props.week_data[1].temperature}
-                                       day={this.props.week_data[1].weekday}
-                                       humidity={this.props.week_data[1].humidity}
-                                       description={this.props.week_data[1].description}/>
-              </div>
-             
-                <div className="ww-div">
-              <Weather className="ww" temperature={this.props.week_data[2].temperature}
-                                       day={this.props.week_data[2].weekday}
-                                       humidity={this.props.week_data[2].humidity}
-                                       description={this.props.week_data[2].description}/>
-                </div>
-                <div className="ww-div">
-              <Weather className="ww" temperature={this.props.week_data[3].temperature}
-                                       day={this.props.week_data[3].weekday}
-                                       humidity={this.props.week_data[3].humidity}
-                                       description={this.props.week_data[3].description}/>
-                </div>
-              </div>
-              <div className="second-row">
-             <div className="ww-div">
-              <Weather className="ww" temperature={this.props.week_data[3].temperature}
-                                       day={this.props.week_data[4].weekday}
-                                       humidity={this.props.week_data[4].humidity}
-                                       description={this.props.week_data[4].description}/>
-                </div>
-            <div className="ww-div">
-              <Weather className="ww" temperature={this.props.week_data[5].temperature}
-                                       day={this.props.week_data[5].weekday}
-                                       humidity={this.props.week_data[5].humidity}
-                                       description={this.props.week_data[5].description}/>
-                </div>
-                <div className="ww-div">
-              <Weather className="ww" temperature={this.props.week_data[6].temperature}
-                                       day={this.props.week_data[6].weekday}
-                                       humidity={this.props.week_data[6].humidity}
-                                       description={this.props.week_data[6].description}/>
-               </div>
-           </div>
-         </div>
-            
+       return(
 
-        );
+
+          <div className="ww-div">
+            {weatherItems}
+            </div>
+          
+        
+        
+      );
     }
 };
 
